bb1d325600df290882d552f68d122c38c236122a
Fede <fedecasta@gmail.com>
2022-05-23 12:03:21 -0300

    Added disabled encryption and block public access resource

--- demo-terraform/aws/simple-ec2-with-s3-access/s3.tf
+++ demo-terraform/aws/simple-ec2-with-s3-access/s3.tf
@@ -15,3 +15,22 @@ resource "aws_s3_bucket_acl" "bucket" {
     bucket = aws_s3_bucket.bucket.id
     acl    = "private"
 }
+
+resource "aws_s3_bucket_public_access_block" "bucket" {
+    bucket = aws_s3_bucket.bucket.id
+
+    block_public_acls   = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+}
+
+# resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
+#     bucket = aws_s3_bucket.bucket.bucket
+
+#     rule {
+#         apply_server_side_encryption_by_default {
+#             sse_algorithm     = "aws:kms"
+#         }
+#     }
+# }
